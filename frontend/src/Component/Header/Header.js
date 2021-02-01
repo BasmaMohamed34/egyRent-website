f9ec2dd336dfe7ee1c28119764b17f24838217f9
Mahmoud Samaha <samaha.mahmoud@yahoo.com>
2021-02-01 16:40:32 +0200

    our logo changes

--- frontend/src/Component/Header/Header.js
+++ frontend/src/Component/Header/Header.js
@@ -11,7 +11,7 @@ function Header() {
             <Link to='/'>
                 <img
                     className="header__icon"
-                    src="https://i.pinimg.com/originals/3c/bf/be/3cbfbe148597341fa56f2f87ade90956.png"
+                    src="./ourLogo.png"
                     alt=""
                 />
             </Link>
