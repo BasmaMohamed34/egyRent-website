9e67638ceec015d349ea1c6153663cda1ea5f069
bishoyrafat <bishoyrafat@yahoo.com>
2021-02-04 20:36:55 +0200

    fix

--- frontend/src/Component/Header/Header.js
+++ frontend/src/Component/Header/Header.js
@@ -20,21 +20,21 @@ function Header() {
         <span className="navbar-toggler-icon"></span>
       </button>
       <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className=" navbar-nav ml-auto pl-3">
+          <ul className=" navbar-nav ml-auto pl-3 bg-dark borderNav">
              <li className="nav-item active">
                  <a className="nav-link" href="#">
-                 <b className = "pr-2" style={{color:"#fff"}}>Home</b>
+                 <b className = "pr-2" style={{color:"#aaa"}}>Home</b>
                    <span className="sr-only">(current)</span>
                  </a>
              </li>
              <li className="nav-item">
                  <a className="nav-link" href="#" target="_blank">
-                   <b style={{color:"#fff"}}>About US</b>
+                   <b style={{color:"#aaa"}}>About US</b>
                  </a>
              </li>
              <li className="nav-item">
                  <a className="nav-link" href="#" target="_blank">
-                   <b className = "pr-3" style={{color:"#fff"}}>Become A Host</b>
+                   <b className = "pr-3" style={{color:"#aaa"}}>Become A Host</b>
                  </a>
              </li>
              <li className="nav-item bg mr-3 mb-3">
