5fbbe08768773b69df1569c4dc670a140ee315ef
Mahmoud Samaha <samaha.mahmoud@yahoo.com>
2021-02-15 00:10:43 +0200

    Update post.js

--- server/controllers/post.js
+++ server/controllers/post.js
@@ -61,6 +61,7 @@ module.exports = {
     console.log(user);
     res.status(201).json(post);
   },
+<<<<<<< Updated upstream
 
   checkAvail: async (req, res, next) => {
     let postID = req.body.post;
@@ -81,3 +82,36 @@ module.exports = {
     } else res.status(200).send("place is available");
   },
 };
+=======
+  
+  checkAvail: async (req, res, next) => {
+    let postID = req.body.post;
+    let x1 = new Date(req.body.checkIn);
+    let x2 = x1.getDate();
+    let y1 = new Date(req.body.checkOut);
+    let y2 = y1.getDate();
+    let dateIn = x1.setDate(x2 + 1);
+    let dateOut = y1.setDate(y2 + 1);
+    let checkin = new Date(dateIn).toISOString();
+    let checkout = new Date(dateOut).toISOString();
+  
+    console.log(checkin)
+    console.log(checkout)
+    const checking = await Reservation.find({ post: postID });
+    console.log(checking)
+    if (checking.length > 0) {
+      checking.forEach(i => {
+        console.log(i)
+        console.log(typeof(JSON.stringify(i.checkIn)));
+        console.log(typeof(checkin));
+        if (JSON.stringify(i.checkIn).slice(1, -1) !== checkin) {
+          console.log(JSON.stringify(i.checkIn).slice(1, -1))
+          res.send("place is available");
+        }
+        else
+          res.send("place is not available");
+      })
+    }
+    }
+};
+>>>>>>> Stashed changes
